53630d988cb3844e32b009731f1261ac92f1e011
Million Integrals <jerry@millionintegrals.com>
2014-05-23 21:03:14 +0100

    Cleaned up regression code.

--- chapter_03/regression.py
+++ chapter_03/regression.py
@@ -2,68 +2,80 @@ __author__ = 'jrx'
 
 import itertools as it
 import numpy as np
+import pandas as pd
+
+import common.base as base
 
 
-def least_squares_regression(X, y):
-    """ Calculate least squares regression """
-    Z = np.linalg.inv(np.dot(X.T, X))
-    betahat = np.dot(np.dot(Z, X.T), y)
-    return betahat
-
-
-def least_squares_regression_with_std_errors(X, y):
-    """ Calculate least squares regression with standard errors calculated """
-    # Get data shape
-    N, p = X.shape
-
-    # Calculate beta coefficients
-    Z = np.linalg.inv(np.dot(X.T, X))
-    betahat = np.dot(np.dot(Z, X.T), y)
-
-    # Calculate standard errors
-    epsilon = y - np.dot(X, betahat)
-    rss = np.dot(epsilon, epsilon)
-    sigma2 = 1.0 / (N - p) * rss
-    sigma = np.sqrt(sigma2)
-    errors = sigma * np.sqrt(np.matrix.diagonal(Z))
-
-    return betahat, errors
-
-
-def best_subset_selection(X, y, size):
-    """ Find the best (in terms of square error) subset of input labels of given sample """
-    N, p = X.shape
-
-    # Remember best values
-    best_combination = None
-    best_error = None
-    best_beta = None
-
-    for combination in it.combinations(range(p-1), size):
-        # Add the intercept
-        indices = [0] + [x + 1 for x in combination]
-        Xsel = X[:, indices]
-        beta = least_squares_regression(Xsel, y)
-
-        residuals = y - np.dot(Xsel, beta)
-        rss = np.dot(residuals, residuals)
-
-        if (best_error is None) or (rss < best_error):
-            best_error = rss
-            best_combination = indices
-            best_beta = beta
-
-    return best_beta, best_combination
-
-
-def test_error(beta, X, y):
+def test_error(model, coords, values):
     """ Calculate the error of the model using methodology from the book """
-    residuals = y - np.dot(X, beta)
+    residuals = values - model.calculate(coords)
     residuals2 = residuals * residuals
 
-    N, p = X.shape
+    n, p = coords.shape
 
     avg_error = np.average(residuals2)
-    std_error = np.std(residuals2, ddof=1) / np.sqrt(N)
+    std_error = np.std(residuals2, ddof=1) / np.sqrt(n)
 
     return avg_error, std_error
+
+
+class LeastSquaresRegression(base.Regression):
+    """ Linear least squares regression """
+
+    def __init__(self, coords, values):
+        super(LeastSquaresRegression, self).__init__()
+
+        # Get data shape
+        n, p = coords.shape
+
+        # Calculate beta coefficients
+        z = np.linalg.inv(np.dot(coords.T, coords))
+        self.betahat = np.dot(np.dot(z, coords.T), values)
+
+        # Calculate standard errors
+        epsilon = values - np.dot(coords, self.betahat)
+        self.rss = np.dot(epsilon, epsilon)
+
+        sigma2 = 1.0 / (n - p) * self.rss
+        self.sigma = np.sqrt(sigma2)
+        self.std_errors = self.sigma * np.sqrt(np.matrix.diagonal(z))
+
+    def calculate(self, samples):
+        return pd.Series(np.dot(samples, self.betahat), index=samples.index)
+
+
+class BestSubsetSelection(base.Regression):
+    """ Best subset selection algorithm """
+
+    def __init__(self, coords, values, size):
+        super(BestSubsetSelection, self).__init__()
+
+        n, p = coords.shape
+
+        # Remember best values
+        best_combination = None
+        best_error = None
+        best_beta = None
+
+        for combination in it.combinations(range(p-1), size):
+            # Add the intercept
+            indices = [0] + [x + 1 for x in combination]
+            x_sel = coords.iloc[:, indices]
+
+            z = np.linalg.inv(np.dot(x_sel.T, x_sel))
+            beta = np.dot(np.dot(z, x_sel.T), values)
+
+            residuals = values - np.dot(x_sel, beta)
+            rss = np.dot(residuals, residuals)
+
+            if (best_error is None) or (rss < best_error):
+                best_error = rss
+                best_combination = indices
+                best_beta = beta
+
+        self.betahat = best_beta
+        self.best_combination = best_combination
+
+    def calculate(self, samples):
+        return pd.Series(np.dot(samples.values[:, self.best_combination], self.betahat), index=samples.index)
